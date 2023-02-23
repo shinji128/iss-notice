5d7eee3eda0e8a592ea295ae5c54e8064f65cd39
miura <s.miura@mof-mof.co.jp>
2023-02-23 15:06:10 +0900

    renderにデプロイ

---
+++ bin/render-build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+yarn install
+yarn build # jsファイルをesbuildでバンドルしているため
+bundle exec rake assets:precompile # cssはsprocketsを使っているため
--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module IssNotice
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << 'https://iss-notice.onrender.com'
   end
 end
